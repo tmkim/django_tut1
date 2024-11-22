93937c296f65e8aee68e1f6e05371403c6b86a22
tmkim <taeminkim13@gmail.com>
2024-11-22 14:05:46 -0500

    tutorial p3 -- set up more views, practice setting urls, namespace, using templates, exceptions, and render

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:q_id>/results/", views.results, name="results"),
+    path("<int:q_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. Welcome to the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+    
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+    # return HttpResponse("Question %s." % question_id)
+
+def results(request, q_id):
+    response = "Results for %s."
+    return HttpResponse(response % q_id)
+
+def vote(request, q_id):
+    return HttpResponse("Voting question %s." % q_id)
