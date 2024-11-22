8e1af7f5757e0e3927894b558e96003b9db9e7eb
tmkim <taeminkim13@gmail.com>
2024-11-21 19:02:47 -0500

    set up postgresql

--- django_tut1/settings.py
+++ django_tut1/settings.py
@@ -74,12 +74,22 @@ WSGI_APPLICATION = 'django_tut1.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'verceldb',
+
+        'USER': 'default',
+
+        'PASSWORD': 'jG1wuxptkc7E',
+
+        'HOST': 'ep-wandering-dream-a4wxdpjb-pooler.us-east-1.aws.neon.tech',
+
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
